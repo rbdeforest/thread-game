d24c913bddeaa98460fc58cf82099a0b01df863b
Ryan DeForest <ryan@red35.co>
2026-02-16 11:41:15 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Thread from './Thread.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Thread />
+  </React.StrictMode>,
+)
